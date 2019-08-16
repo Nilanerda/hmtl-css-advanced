b2a9dc10e949203233636e2ea036ff97422b2050
Yana <zzizzu@ukr.net>
2019-08-16 22:02:46 +0300

    delete useless string from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', mysass);
 function mysass() {
     return gulp.src('./*.scss')
       .pipe(sass().on('error', sass.logError))
